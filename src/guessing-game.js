866415e5dd2e1a277eab71ed4298591c62169041
denismezhenin <dzianismiazhenin@gmail.com>
2022-08-16 16:49:37 +0300

    finish

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+this.rightNum = max;
+this.leftNum = min;
     }
 
     guess() {
-
+return this.newRangeBorder = Math.round((this.rightNum + this.leftNum) /2)
     }
 
     lower() {
-
+    return this.rightNum = this.newRangeBorder
     }
 
     greater() {
-
+     return this.leftNum = this.newRangeBorder
     }
 }
 
